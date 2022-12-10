20c82815943cee1d1f6fe95e1123df5d2c16cb44
Jean Kistenmacher <jean.kistenmacher@b2wdigital.com>
2022-12-10 18:06:14 -0300

    feat: Maquina Norma

--- index.js
+++ index.js
@@ -22,14 +22,16 @@ class Reg {
 let regMap = {}
 
 let inserir = prompt('Deseja inserir registrador? S|N ')
+
 while (inserir != 'N') {
   let regValue = prompt('Insira Registrador/Valor? Reg/Val ')
   let registrador = regValue.split('/')
-  regMap[registrador[0]] = new Reg(registrador[1])
+  regMap[registrador[0]] = new Reg(Number(registrador[1]))
 
   inserir = prompt('Deseja inserir registrador? S|N ')
 }
-console.log(regMap)
+console.log(`\n\nREGISTRADORES:\n`)
+printRegistradores(regMap)
 
 function syncReadFile(filename) {
   const contents = readFileSync(filename, 'utf-8')
@@ -37,4 +39,55 @@ function syncReadFile(filename) {
   return contentArray
 }
 
-syncReadFile('./programa.txt')
+const programa = syncReadFile('./programa.txt')
+console.log(`PROGRAMA:\n`)
+console.log(programa)
+
+let programaRodando = true
+let linha = 0
+console.log('\nINICIA PROGRAMA\n')
+while (programaRodando) {
+  instrucao = programa[linha].split(' ')
+  console.log(`${programa[linha]}`)
+  switch (instrucao[1]) {
+    case 'se':
+      const teste = instrucao[2].split('_')
+      if (teste[0] === 'zero') {
+        let registrador = teste[1]
+        const resultadoTeste = regMap[registrador].equalZero()
+
+        if (resultadoTeste) {
+          linha = instrucao[5] - 1
+          console.log(`${instrucao[4]} ${instrucao[5]}\n`)
+        } else {
+          linha = instrucao[8] - 1
+          console.log(`${instrucao[7]} ${instrucao[8]}\n`)
+        }
+      }
+
+      break
+    case 'faça':
+      const operacao = instrucao[2].split('_')
+      let registrador = operacao[1]
+      if (operacao[0] === 'ad') {
+        regMap[registrador].add()
+      } else if (operacao[0] === 'sub') {
+        regMap[registrador].sub()
+      }
+      linha = instrucao[4] - 1
+      printRegistradores(regMap)
+      break
+  }
+  if (programa.length < linha) {
+    programaRodando = false
+    console.log(`\n\nValores Finais:`)
+    printRegistradores(regMap)
+  }
+}
+
+function printRegistradores(obj) {
+  Object.keys(obj).forEach(key => {
+    console.log(key, obj[key])
+  })
+  console.log('\n')
+}
