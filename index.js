c2b049d27b6e356f4863422ef2a3a5a1a25a483e
Jean Kistenmacher <jean.kistenmacher@b2wdigital.com>
2022-12-10 16:34:09 -0300

    feat: inserir registradores

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 const prompt = require('prompt-sync')()
-let registradores = prompt('Número de registradores: ')
-console.log(`registradores informados: ${Number(registradores)}`)
+class Reg {
+  constructor(value) {
+    this.value = value
+  }
+
+  get getValue() {
+    return this.value
+  }
+  add() {
+    this.value++
+  }
+  sub() {
+    this.value--
+  }
+  equalZero() {
+    return this.value === 0
+  }
+}
+
+let regMap = {}
+
+let inserir = prompt('Deseja inserir registrador? S|N ')
+while (inserir != 'N') {
+  let regValue = prompt('Insira Registrador/Valor? Reg/Val ')
+  let registrador = regValue.split('/')
+  regMap[registrador[0]] = new Reg(registrador[1])
+
+  inserir = prompt('Deseja inserir registrador? S|N ')
+}
+
+console.log(regMap)
